f2f00c9094afb9e622b1d5112a7b6c37c0fb508b
Anwesh43 <anweshthecool0@gmail.com>
2021-03-19 03:05:31 +0530

    adding constants and libraries

--- pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
+++ pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
@@ -1,2 +1,26 @@
 package com.example.pathsquareview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#2196F3",
+    "#BF360C",
+    "#01579B"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.6f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
