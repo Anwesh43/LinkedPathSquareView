2df02d4b9545f9ddb7a061147693df6b8d530ce8
Anwesh43 <anweshthecool0@gmail.com>
2021-03-19 03:14:00 +0530

    adding Canvas extension methods

--- pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
+++ pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
@@ -29,3 +29,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawPathSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h /2)
+    drawRect(RectF(-size / 2, -size * sf1 - (h - size) * sf3, size / 2, 0f), paint)
+    for (j in 0..1) {
+        save()
+        translate(-size / 2 + size * j, -size)
+        drawLine(0f, 0f, 0f, -(h - size) * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawPSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPathSquare(scale, w, h, paint)
+}
