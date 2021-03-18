56b9bc42c5d4132214e4eb0a4e32d97c72b5e860
Anwesh43 <anweshthecool0@gmail.com>
2021-03-19 03:29:03 +0530

    adding PathSquare class

--- pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
+++ pathsquareview/src/main/java/com/example/pathsquareview/PathSquareView.kt
@@ -141,7 +141,7 @@ class PathSquareView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNect(dir : Int, cb : () -> Unit) : PSNode {
+        fun getNext(dir : Int, cb : () -> Unit) : PSNode {
             var curr : PSNode? = prev
             if (dir == 1) {
                 curr = next
@@ -151,6 +151,29 @@ class PathSquareView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class PathSquare(var i : Int, val state : State = State()) {
+
+        private var curr : PSNode = PSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
